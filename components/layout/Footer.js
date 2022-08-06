c669661eded6e0a437809b02a20d8b74c1e6ee20
elvis <cenelvis@gmail.com>
2022-08-06 17:02:43 -0400

    footer work

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -2,52 +2,65 @@ import classes from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={classes.main}>
-      <div className={classes.container}>
-        <div className={classes.row}>
-          {/* {Col1} */}
-          <div className={classes.col}>
-            <h4>About Us</h4>
-            <ul className={classes.list}>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-            </ul>
-          </div>
-          {/* {Col2} */}
-          <div className={classes.col}>
-            <h4>Contact Us</h4>
-            <ul className={classes.list}>
-              <li>e-mail</li>
-              <li>number</li>
-            </ul>
-          </div>
-          {/* {Col3} */}
-          <div className={classes.col}>
-            <h4>Media</h4>
-            <ul className={classes.list}>
-              <li>Twitter</li>
-              <li>FaceBook</li>
-              <li>Instagram</li>
-            </ul>
-          </div>
-          {/* {Col4} */}
-          <div className={classes.col}>
-            <h4>More Stuff</h4>
-            <ul className={classes.list}>
-              <li>More things</li>
-            </ul>
-          </div>
+    <footer className={classes.main}>
+      <div className={classes.row}>
+        <div className={classes.col}>
+          <div className={classes.title}>About Us</div>
+          <a href="#" className={classes.link}>
+            1
+          </a>
+          <a href="#" className={classes.link}>
+            2
+          </a>
+          <a href="#" className={classes.link}>
+            3
+          </a>
         </div>
-        <hr />
-        <div className={classes.row}>
-          <p className={classes.small}>
-            &copy;{new Date().getFullYear()} Satswap | All right reserved |
-            Terms Of Service | Privacy
-          </p>
+        <div className={classes.col}>
+          <div className={classes.title}>Services</div>
+          <a href="#" className={classes.link}>
+            1
+          </a>
+          <a href="#" className={classes.link}>
+            2
+          </a>
+          <a href="#" className={classes.link}>
+            3
+          </a>
+        </div>
+        <div className={classes.col}>
+          <div className={classes.title}>Contact Us</div>
+          <a href="#" className={classes.link}>
+            1
+          </a>
+          <a href="#" className={classes.link}>
+            2
+          </a>
+          <a href="#" className={classes.link}>
+            3
+          </a>
+        </div>
+        <div className={classes.col}>
+          <div className={classes.title}>Social</div>
+          <a href="#" className={classes.link}>
+            Facebook
+          </a>
+          <a href="#" className={classes.link}>
+            Instagram
+          </a>
+          <a href="#" className={classes.link}>
+            Twitter
+          </a>
         </div>
       </div>
-    </div>
+      <hr />
+      <div className={classes.row}>
+        <p className={classes.small}>
+          &copy;{new Date().getFullYear()} Satswap | All right reserved | Terms
+          Of Service | Privacy
+        </p>
+      </div>
+    </footer>
   );
 };
 
